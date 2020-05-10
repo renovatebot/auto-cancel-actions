09d3b8c4d08669253b48893d5ff0f80366666675
Michael Kriese <michael.kriese@visualon.de>
2020-05-10 17:32:00 +0200

    fix: catch errors

--- src/index.ts
+++ src/index.ts
@@ -44,83 +44,89 @@ Probot.run((app: Application) => {
     const id = context.payload.check_run.id;
     const repo = context.repo();
 
-    const { data: check } = await context.github.checks.get({
-      ...repo,
-      check_run_id: id,
-    });
-    if (check.app.slug !== 'github-actions') {
-      // ignore
-      return;
-    }
+    context.log('Running check:', chalk.grey(id));
 
-    const wfres = await getWorkflowId(context, repo, id);
-    if (!wfres) {
-      // ignore
-      return;
-    }
-
-    const { run_id, run_number, ...wf } = wfres;
-
-    const { data: runs } = await context.github.actions.listWorkflowRuns({
-      ...repo,
-      ...wf,
-    });
-
-    const event = context.event;
-
-    for (const run of runs.workflow_runs) {
-      if (run.id === run_id) {
-        context.log(chalk.yellow('Ignore me'), ':', chalk.grey(run.html_url));
-        continue;
+    try {
+      const { data: check } = await context.github.checks.get({
+        ...repo,
+        check_run_id: id,
+      });
+      if (check.app.slug !== 'github-actions') {
+        // ignore
+        return;
       }
 
-      if (run.run_number > run_number) {
-        context.log(
-          chalk.yellow('Ignore newer'),
-          ':',
-          chalk.grey(run.html_url)
-        );
-        continue;
+      const wfres = await getWorkflowId(context, repo, id);
+      if (!wfres) {
+        // ignore
+        return;
       }
-      if (run.status !== 'in_progress' && run.status !== 'queued') {
-        context.log(
-          chalk.yellow(`Ignore state`),
-          run.status,
-          ':',
-          chalk.grey(run.html_url)
-        );
-        continue;
-      }
-      if (run.event !== event) {
-        context.log(
-          chalk.yellow(`Ignore event`),
-          run.event,
-          ':',
-          chalk.grey(run.html_url)
-        );
-        continue;
-      }
-      if (dryRun) {
-        context.log.warn(
-          chalk.yellow('[DRY_RUN]'),
-          chalk.blue('Would cancel'),
+
+      const { run_id, run_number, ...wf } = wfres;
+
+      const { data: runs } = await context.github.actions.listWorkflowRuns({
+        ...repo,
+        ...wf,
+      });
+
+      const event = context.event;
+
+      for (const run of runs.workflow_runs) {
+        if (run.id === run_id) {
+          context.log(chalk.yellow('Ignore me'), ':', chalk.grey(run.html_url));
+          continue;
+        }
+
+        if (run.run_number > run_number) {
+          context.log(
+            chalk.yellow('Ignore newer'),
+            ':',
+            chalk.grey(run.html_url)
+          );
+          continue;
+        }
+        if (run.status !== 'in_progress' && run.status !== 'queued') {
+          context.log(
+            chalk.yellow(`Ignore state`),
+            run.status,
+            ':',
+            chalk.grey(run.html_url)
+          );
+          continue;
+        }
+        if (run.event !== event) {
+          context.log(
+            chalk.yellow(`Ignore event`),
+            run.event,
+            ':',
+            chalk.grey(run.html_url)
+          );
+          continue;
+        }
+        if (dryRun) {
+          context.log.warn(
+            chalk.yellow('[DRY_RUN]'),
+            chalk.blue('Would cancel'),
+            ':',
+            run.id,
+            chalk.grey(run.html_url)
+          );
+          continue;
+        }
+
+        context.log.info(
+          chalk.blue('Cancel:'),
           ':',
           run.id,
           chalk.grey(run.html_url)
         );
-        continue;
+        await context.github.actions.cancelWorkflowRun({
+          ...repo,
+          run_id: run.id,
+        });
       }
-
-      context.log.info(
-        chalk.blue('Cancel:'),
-        ':',
-        run.id,
-        chalk.grey(run.html_url)
-      );
-      await context.github.actions.cancelWorkflowRun({
-        ...repo,
-        run_id: run.id,
-      });
+    } catch (e) {
+      context.log.error('unexpected error', e);
     }
   });
 });
