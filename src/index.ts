a4020634a0f6697a959e25189390ecedcc4ca18e
Michael Kriese <michael.kriese@visualon.de>
2020-05-12 14:56:36 +0200

    fix: promise

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,7 @@
-import { Probot, Application } from 'probot';
+import { Application, Probot } from 'probot';
 import { Runner } from './runner';
 
 Probot.run((app: Application) => {
   app.log('App loaded');
-  app.on('check_run.created', async (context) => {
-    new Runner(context).run();
-  });
+  app.on('check_run.created', (context) => new Runner(context).run());
 });
